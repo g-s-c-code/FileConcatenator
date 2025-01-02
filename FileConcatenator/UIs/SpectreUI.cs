9e9717fee03aa6c5e0c66bab9c5b9fbb7603ed2b
GSC <gsc.code@gmail.com>
2025-01-02 08:17:18 +0100

    Refactored UI.

--- FileConcatenator/UIs/SpectreUI.cs
+++ FileConcatenator/UIs/SpectreUI.cs
@@ -1,29 +1,52 @@
 ﻿using Spectre.Console;
 using Spectre.Console.Rendering;
+
 public class SpectreUI
 {
 	private const string BoldFormat = "bold";
 	private const string UnderlineFormat = "underline";
-	private readonly Color _textColor = Color.White;
-	private readonly Color _headerColor = Color.LightSkyBlue1;
+	private readonly Color TextColor = Color.White;
+	private readonly Color HeaderColor = Color.LightSkyBlue1;
 
+	// Utility Methods
 	public void Clear() => AnsiConsole.Clear();
+
 	public void ShowMessage(string message) => AnsiConsole.Write(message);
+
 	public void ShowMessageAndWait(string message)
 	{
 		ShowMessage(message);
 		Console.ReadKey();
 	}
-	public string GetInput(string input) => AnsiConsole.Ask<string>(input);
 
-	public string Text(string text, Color? color = null)
-		=> FormatText(text, color ?? _textColor, BoldFormat);
+	public string GetInput(string prompt) => AnsiConsole.Ask<string>(prompt);
 
-	public string Header(string text)
-		=> FormatText(text, _headerColor, BoldFormat, UnderlineFormat).ToUpper();
+	// Formatting Methods
+	public string Text(string content, Color? color = null)
+		=> FormatText(content, color ?? TextColor, BoldFormat);
 
-	private string FormatText(string text, Color color, params string[] formats)
-		=> $"[{string.Join(" ", formats)} {color}]{text}[/]";
+	public string Header(string content)
+		=> FormatText(content, HeaderColor, BoldFormat, UnderlineFormat).ToUpper();
+
+	private string FormatText(string content, Color color, params string[] formats)
+		=> $"[{string.Join(" ", formats)} {color}]{content}[/]";
+
+	// Rendering Methods
+	public void MainLayout(string currentDirectory, string commands, string settingsHeaders,
+		string currentSettings, IEnumerable<string> directoriesTree, IEnumerable<string> filesTree)
+	{
+		var leftColumn = CreateLeftColumn(settingsHeaders, currentSettings, commands);
+		var rightColumn = DirectoryContentUI(CurrentDirectoryPathUI(currentDirectory), CurrentDirectoryContentUI(directoriesTree, filesTree));
+
+		var mainLayout = new Table()
+			.AddColumns(
+				new TableColumn(leftColumn),
+				new TableColumn(rightColumn))
+			.BorderColor(TextColor)
+			.Border(TableBorder.Horizontal);
+
+		AnsiConsole.Write(mainLayout);
+	}
 
 	public IRenderable DisplayTree(string header, IEnumerable<string> items)
 	{
@@ -31,26 +54,42 @@ public class SpectreUI
 		{
 			Style = new Style(foreground: Color.RosyBrown)
 		};
+
 		foreach (var item in items)
 		{
 			tree.AddNode(Text(Markup.Escape(item)));
 		}
+
 		return tree;
 	}
 
-	public void MainLayout(string currentDirectory, string commands, string settingsHeaders,
-		string currentSettings, IEnumerable<string> directoriesTree, IEnumerable<string> filesTree)
+	// Private Helper Methods
+	private Table CreateLeftColumn(string settingsHeaders, string currentSettings, string commands)
 	{
-		var leftColumn = CreateLeftColumn(settingsHeaders, currentSettings, commands);
-		var rightColumn = DirectoryContentUI(CurrentDirectoryPathUI(), CurrentDirectoryContentUI(directoriesTree, filesTree));
-		var mainLayout = new Table()
-			.AddColumns(
-				new TableColumn(leftColumn),
-				new TableColumn(rightColumn))
-			.BorderColor(_textColor)
-			.Border(TableBorder.Horizontal);
-		AnsiConsole.Write(mainLayout);
+		var settingsSection = CreateSettingsSection(settingsHeaders, currentSettings);
+		var commandsSection = CreateCommandsSection(commands);
+
+		return new Table()
+			.AddColumn(new TableColumn(Header("Current Settings:")))
+			.AddRow(settingsSection)
+			.AddEmptyRow()
+			.AddRow(Header("Commands:"))
+			.AddRow(commandsSection)
+			.Border(TableBorder.None)
+			.Width(50);
 	}
+
+	private Table CreateSettingsSection(string settingsHeaders, string currentSettings)
+		=> new Table()
+			.AddColumns(
+				new TableColumn(Text(settingsHeaders)),
+				new TableColumn(Text(currentSettings)))
+			.Border(TableBorder.None);
+
+	private Table CreateCommandsSection(string commands)
+		=> new Table()
+			.AddColumn(new TableColumn(Text(commands)))
+			.Border(TableBorder.None);
 
 	private Table CreateRightColumn(string currentDirectory, IEnumerable<string> directoriesTree, IEnumerable<string> filesTree)
 	{
@@ -64,27 +103,27 @@ public class SpectreUI
 			.Border(TableBorder.None);
 	}
 
-	private Panel DirectoryContentUI(Panel currentDirectoryPathUI, Table currentDirectoryContentUI)
+	private Panel CurrentDirectoryPathUI(string currentDirectory)
 	{
-		return new Panel(new Rows([currentDirectoryPathUI, currentDirectoryContentUI]))
+		var textPath = new TextPath(currentDirectory.ToUpper())
+			.SeparatorColor(Color.RosyBrown)
+			.RootColor(TextColor)
+			.StemColor(TextColor)
+			.LeafColor(TextColor);
+
+		return new Panel(textPath)
+		{
+			Border = BoxBorder.None,
+		};
+	}
+
+	private Panel DirectoryContentUI(Panel directoryPathPanel, Table directoryContentTable)
+	{
+		return new Panel(new Rows(directoryPathPanel, directoryContentTable))
 		{
 			BorderStyle = Color.LightSkyBlue1,
 			Header = new PanelHeader("[[ Current Directory ]]".ToUpper()),
 			Padding = new Padding(0, 1, 0, 0),
-		};
-	}
-
-	private Panel CurrentDirectoryPathUI()
-	{
-		var currentDirectory = new TextPath(Directory.GetCurrentDirectory().ToUpper())
-			.RootColor(Color.White)
-			.SeparatorColor(Color.RosyBrown)
-			.StemColor(Color.White)
-			.LeafColor(Color.White);
-
-		return new Panel(currentDirectory)
-		{
-			Border = BoxBorder.None,
 		};
 	}
 
@@ -102,46 +141,4 @@ public class SpectreUI
 
 		return table;
 	}
-
-	private Panel CurrentDirectoryPathUI(string currentDirectory)
-	{
-		var textPath = new TextPath(currentDirectory.ToUpper())
-			.SeparatorColor(Color.RosyBrown)
-			.RootColor(_textColor)
-			.StemColor(_textColor)
-			.LeafColor(_textColor);
-
-		return new Panel(textPath)
-		{
-			Padding = new Padding(0),
-			Border = BoxBorder.None,
-		};
-	}
-
-	private Table CreateLeftColumn(string settingsHeaders, string currentSettings, string commands)
-	{
-		var upperSection = CreateSettingsSection(settingsHeaders, currentSettings);
-		var lowerSection = CreateCommandsSection(commands);
-
-		return new Table()
-			.AddColumn(new TableColumn(Header("Current Settings:")))
-			.AddRow(upperSection)
-			.AddEmptyRow()
-			.AddRow(Header("Commands:"))
-			.AddRow(lowerSection)
-			.Border(TableBorder.None)
-			.Width(50);
-	}
-
-	private Table CreateSettingsSection(string settingsHeaders, string currentSettings)
-		=> new Table()
-			.AddColumns(
-				new TableColumn(Text(settingsHeaders)),
-				new TableColumn(Text(currentSettings)))
-			.Border(TableBorder.None);
-
-	private Table CreateCommandsSection(string commands)
-		=> new Table()
-			.AddColumn(new TableColumn(Text(commands)))
-			.Border(TableBorder.None);
 }
